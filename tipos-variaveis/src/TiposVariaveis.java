a97ddb7a0a5821f74d2b74b257129b8aed6ef6d3
Antonio Tiburtino <antonio_ammt@hotmail.com>
2024-11-23 20:05:32 -0300

    Criando o projeto para exemplos de tipos e variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,11 +1,5 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        int num1 = 10;
-        int nume2 = 30;
-
-        int soma = num1 + nume2;
-
-        System.out.println(soma);
-        System.out.println("Hello, World!");
+        System.out.println("Olá Mundo");
     }
 }
